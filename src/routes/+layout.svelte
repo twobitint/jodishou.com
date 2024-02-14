41050e2ac7a5667ae800745db4843e6ec5eeafa2
Justin Labak <jtlabak@gmail.com>
2024-02-13 17:01:15 -0800

    feat: rough draft first homepage

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script>
+  import "../app.css";
+</script>
+
+<div class="container mx-auto">
+  <header class="flex flex-row justify-between my-12">
+    <h1 class="text-xl">
+      <strong>Jodi Shou</strong>
+      <small class="text-slate-500 ml-4">Marketing Strategy & Artist Manager</small>
+    </h1>
+    <nav class="grid gap-4 grid-flow-col text-slate-500">
+      <a href="/"><strong class="text-black">Artist Management</strong></a>
+      <a href="/about">FUGA</a>
+      <a href="/contact">About</a>
+      <a href="">Tunes</a>
+      <a href="">Mood</a>
+    </nav>
+  </header>
+  <slot />
+</div>
+
+<!-- <style lang="postcss">
+  nav.a {
+    
+  }
+  </style> -->
---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+
+<div class="grid grid-cols-2 gap-6 grayscale">
+    <figure>
+        <img src="https://source.unsplash.com/random/668x444/?music&1" alt="Music Industry Thing" />
+    </figure>
+
+    <figure>
+        <img src="https://source.unsplash.com/random/668x444/?music&2" alt="Music Industry Thing" />
+    </figure>
+
+    <figure>
+        <img src="https://source.unsplash.com/random/668x444/?music&3" alt="Music Industry Thing" />
+    </figure>
+
+    <figure>
+        <img src="https://source.unsplash.com/random/668x444/?music&4" alt="Music Industry Thing" />
+    </figure>
+
+    <figure>
+        <img src="https://source.unsplash.com/random/668x444/?music&5" alt="Music Industry Thing" />
+    </figure>
+
+    <figure>
+        <img src="https://source.unsplash.com/random/668x444/?music&6" alt="Music Industry Thing" />
+    </figure>
+</div>
+
+<style lang="postcss">
+:global(html) {
+    background-color: theme(colors.gray.100);
+}
+</style>
